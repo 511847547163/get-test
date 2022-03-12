6ee2b4dc078bb7728c744a270ebda61d3640c76b
Layne <Layne@atguigu.com>
2022-03-12 21:01:03 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("heel git");
         System.out.println("heel git2");
+        System.out.println("heel git2");
     }
 }
