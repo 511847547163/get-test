d6440a3713826780de8062aba51bbce411a2bc9a
511847547163 <90202770+511847547163@users.noreply.github.com>
2022-03-12 22:44:41 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("heel git3");
         System.out.println("master-test");
         System.out.println("push-test");
-
+         System.out.println("pull-test");
     }
 }
